eaf930f1ae68a7b7b46d7ab096ffd8f288bafd0c
Saeid Zorofchi <saeid.zorofchi@gmail.com>
2025-10-16 19:43:18 +0330

    add Exception section

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,7 @@ import org.example.multiThread.Thread0to100;
 import org.example.multiThread.ThreadAtoZ;
 import org.example.serialization.User;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -66,6 +63,7 @@ public class Main {
         System.out.println("//////////////// START Thread section block ////////////////");
         System.out.println();
         {
+
             System.out.println("print on main method");
             MyThread thread1 = new MyThread();
             thread1.start();
@@ -77,21 +75,60 @@ public class Main {
             System.out.println();
 
 
-
+            
 
 
             Thread t1 = new Thread(new ThreadAtoZ());
             t1.start();
             t1.setPriority(7);
+            Thread currentThread = Thread.currentThread();
+            System.out.println(currentThread.getId() + 1000);
+            System.out.println(currentThread.getName());
             new Thread0to100().start();
             new Thread(new ThreadAtoZ()).start();
             new Thread0to100().start();
             t1.join();
 
+        }
+        System.out.println();
+        System.out.println("//////////////// END Thread section block ////////////////\n");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+//////////////// END Thread section block ////////////////
+
+
+
+
+//////////////// START Exception section block ////////////////
+        System.out.println("//////////////// START Exception section block ////////////////");
+        System.out.println();
+        {
+            // Checked Exception
+            File file = new File("E:/Saeid Zorofchi - Java Developer.pdf");
+            try {
+                FileReader fileReader = new FileReader(file);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            // UnChecked Exception
+            try {
+                int a = 25;
+                int b = 0;
+                int c = a/b;
+                System.out.println(c);
+            }catch (Exception e){
+                System.out.println("Error : "+ e.getMessage());
+            }finally {
+                System.out.println("The End");
+            }
+
+
+
 
 
         }
-
 
     }
 }
--- src/main/java/org/example/multiThread/Thread0to100.java
+++ src/main/java/org/example/multiThread/Thread0to100.java
@@ -1,9 +1,13 @@
 package org.example.multiThread;
 
-public class Thread0to100 extends Thread{
-    public void run(){
-        for (int i = 0 ; i <= 50 ; i++){
+public class Thread0to100 extends Thread {
+    public void run() {
+
+        for (int i = 0; i <= 50; i++) {
             System.out.println(i);
         }
+        Thread currentThread = Thread.currentThread();
+        System.out.println(currentThread.getId() + 1000);
+        System.out.println(currentThread.getName());
     }
 }
